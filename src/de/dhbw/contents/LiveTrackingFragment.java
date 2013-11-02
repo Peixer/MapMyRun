845b7f20f1fcf120095c4e42f31d934a0f9dd797
Timea Magyar <timea.magyar@mhm-hr.com>
2013-11-02 20:36:47 +0100

    calculation current longitude and latidude

--- src/de/dhbw/contents/LiveTrackingFragment.java
+++ src/de/dhbw/contents/LiveTrackingFragment.java
@@ -5,12 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.actionbarsherlock.app.SherlockFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 
 import de.dhbw.container.R;
 import de.dhbw.container.R.layout;
 
 public class LiveTrackingFragment extends SherlockFragment {
-	public static final String ARG_PLANET_NUMBER = "planet_number";
+	private MapView mMapView;
 
 	public LiveTrackingFragment() {
 		// Empty constructor required for fragment subclasses
@@ -19,8 +25,12 @@ public class LiveTrackingFragment extends SherlockFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.live_tracking_fragment, container,
+		// inflat and return the layout
+		View v = inflater.inflate(R.layout.live_tracking_fragment, container,
 				false);
-		return rootView;
+
+		
+		return v;
+
 	}
 }
