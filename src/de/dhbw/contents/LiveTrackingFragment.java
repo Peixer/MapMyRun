c9de2ea0cfcc884e33eabf4b7ee4fa1fa2e6af50
Timea Magyar <timea.magyar@mhm-hr.com>
2013-11-08 11:29:13 +0100

    modified tracking fragment

--- src/de/dhbw/contents/LiveTrackingFragment.java
+++ src/de/dhbw/contents/LiveTrackingFragment.java
@@ -5,26 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.actionbarsherlock.app.SherlockFragment;
-<<<<<<< HEAD
-import com.google.android.gms.maps.MapView;
-=======
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
->>>>>>> c08deecc2aea8c282d014c0f6fa314c4c1b8622f
+
+
+
+
 
 import de.dhbw.container.R;
 
 public class LiveTrackingFragment extends SherlockFragment {
-<<<<<<< HEAD
-	MapView m;
-=======
-	private MapView mMapView;
 
->>>>>>> c08deecc2aea8c282d014c0f6fa314c4c1b8622f
+
 	public LiveTrackingFragment() {
 		// Empty constructor required for fragment subclasses
 	}
@@ -33,43 +23,9 @@ public class LiveTrackingFragment extends SherlockFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// inflat and return the layout
-		View v = inflater.inflate(R.layout.live_tracking_fragment, container,
+		View rootView = inflater.inflate(R.layout.live_tracking_fragment, container,
 				false);
-<<<<<<< HEAD
-		m = (MapView) rootView.findViewById(R.id.mapView);
-		m.onCreate(savedInstanceState);
-		
-		
 		return rootView;
-=======
 
-		
-		return v;
-
->>>>>>> c08deecc2aea8c282d014c0f6fa314c4c1b8622f
 	}
-	
-	@Override
-    public void onResume() {
-        super.onResume();
-        m.onResume();
-    }
-		     
-    @Override
-    public void onPause() {
-        super.onPause();
-        m.onPause();
-    }
-     
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        m.onDestroy();
-    }
-     
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        m.onLowMemory();
-    }
 }
