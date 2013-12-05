596b684f3d612ca715764c725f51d863165ad8fe
Timea Magyar <timea.magyar@mhm-hr.com>
2013-12-05 10:08:41 +0100

    refactor and comment code

--- src/de/dhbw/contents/LiveTrackingFragment.java
+++ src/de/dhbw/contents/LiveTrackingFragment.java
@@ -29,8 +29,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.actionbarsherlock.app.SherlockFragment;
 
 import de.dhbw.container.MenuContainerActivity;
@@ -49,40 +47,43 @@ public class LiveTrackingFragment extends SherlockFragment {
 
 	private Context mContext;
 	private GPSTracker gps;
-    int i;
     private DataBaseHandler db;
     private View mView;
-    MyItemizedOverlay myItemizedOverlay;
     private LinearLayout mWorkoutLayout;
-    private MapView mMapView;
     private ListView mListView;
-    private Button trackingButton;
     
     public List<DistanceSegment> mSegmentList = new ArrayList<DistanceSegment>();
     
+    //leerer Konstruktor
 	public LiveTrackingFragment() {
-		// Empty constructor required for fragment subclasses
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// inflate and return the layout
+		// Layout erstellen
 		View v = inflater.inflate(R.layout.live_tracking_fragment, container,
 				false);
 		
+		//Container Acivity
 		mContext = getActivity();
 		
+		//Live Tracking Layout anzeigen
 		mWorkoutLayout = (LinearLayout) v.findViewById(R.id.workout_layout);
 		mWorkoutLayout.setVisibility(View.VISIBLE);
-		mMapView = (MapView) v.findViewById(R.id.mapview);
+		
+		//Karte ausblenden
+		MapView mMapView = (MapView) v.findViewById(R.id.mapview);
 		mMapView.setVisibility(View.GONE);
+		
+		//(Tracking-)Kategorieliste (Dauer,Distanz etc.) ausblenden
 		mListView = (ListView) v.findViewById(R.id.category_list);
 		mListView.setVisibility(View.GONE);
 		
 		db = new DataBaseHandler(mContext);
 
-		trackingButton = (Button) v.findViewById(R.id.tracking);
+		//Button zum Starten, Anhalten und Auswerten von einer Live-Tracking
+		Button trackingButton = (Button) v.findViewById(R.id.tracking);
 		trackingButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
@@ -93,6 +94,7 @@ public class LiveTrackingFragment extends SherlockFragment {
 		return v;
 	}
 	
+	//TODO comment
 	public void onBackPressed()
 	{
 		if (mListView.getVisibility() == View.VISIBLE)
@@ -102,6 +104,7 @@ public class LiveTrackingFragment extends SherlockFragment {
 		}
 	}
 	
+	//TODO comment
 	@Override
 	public void onResume() {
 		// Set List
@@ -110,10 +113,33 @@ public class LiveTrackingFragment extends SherlockFragment {
 		super.onResume();
 	}
 	
+	//TODO comment
+	public void formatCategoryHeadline(AnalysisCategory ac, TextView valueView ){
+		String format = ac.getFormat();
+		
+		if (format.equals("hh:mm:ss"))
+			valueView.setText("00:00:00");
+		else if (format.equals("km"))
+			valueView.setText("0");
+		else if (format.equals("m"))
+			valueView.setText("0");
+		else if (format.equals("kcal"))
+			valueView.setText("0");
+		else if (format.equals("km/h"))
+			valueView.setText("0,0");	
+		else if (format.equals("hh:mm"))
+		{
+			if (!ac.getName().equals("Zeit"))
+				valueView.setText("00:00");
+		}
+
+	}
+	
+	//TODO comment
 	public void setList()
 	{
 		
-		for (i=0; i<7; i++)
+		for (int i=0; i<7; i++)
 		{
 			AnalysisCategory ac = db.getAnalysisCategoryById(db.getCategoryIdByPosition(i+1));
 			if (ac == null)
@@ -123,29 +149,15 @@ public class LiveTrackingFragment extends SherlockFragment {
 			View listElement = mView.findViewById(viewId);
 			listElement.setOnClickListener(new CustomListOnClickListener());
 			
-			String format = ac.getFormat();
 			TextView valueView = ((TextView) listElement.findViewById(R.id.live_tracking_element_value_text));
 			db = new DataBaseHandler(mContext);
 			List <Coordinates> listContents = new ArrayList<Coordinates>();
 			listContents = db.getAllCoordinatePairs();
 			
-			if (format.equals("hh:mm:ss"))
-				valueView.setText("00:00:00");
-			else if (format.equals("km"))
-				valueView.setText("0");
-			else if (format.equals("m"))
-				valueView.setText("0");
-			else if (format.equals("kcal"))
-				valueView.setText("0");
-			else if (format.equals("km/h"))
-				valueView.setText("0,0");	
-			else if (format.equals("hh:mm"))
-			{
-				if (!ac.getName().equals("Zeit"))
-					valueView.setText("00:00");
-			}
+			formatCategoryHeadline(ac, valueView);
+
 			
-			//Werte zuordnen
+			//Tracking Kategorien Werte zuordnen
 			switch(ac.getId())
 			{
 				case 1:		//Dauer
@@ -160,7 +172,7 @@ public class LiveTrackingFragment extends SherlockFragment {
 				case 4:		//Hoehenmeter abwaerts
 					valueView.setText(String.valueOf(TrackService.calcDescent(listContents)));
 					break;
-				case 5:		//Kalorien
+				case 5:		//Verbrannte Kalorien
 					valueView.setText(String.valueOf(TrackService.calcCaloriesBurned(listContents)));
 					break;
 				case 6:		//Durchschnittsgeschwindigkeit
@@ -175,14 +187,15 @@ public class LiveTrackingFragment extends SherlockFragment {
 					break;
 			}
 			
+			//Zu Karegorien icons laden
 			int imageId = getResources().getIdentifier(ac.getImageName(), "drawable", mContext.getPackageName());
 			((ImageView) listElement.findViewById(R.id.live_tracking_element_value_icon)).setImageResource(imageId);
-						
+			//Zu Kategorien Ueberschriften laden		
 			((TextView) listElement.findViewById(R.id.live_tracking_element_name)).setText(ac.getName() + " (" + ac.getFormat() + ")");
 		
 		}
 	}
-	
+	//TODO comment
 	private class CustomListOnClickListener implements View.OnClickListener
 	{
 		private List<AnalysisCategory> mCategoryList;
@@ -208,6 +221,7 @@ public class LiveTrackingFragment extends SherlockFragment {
 			
 		}	
 		
+		//TODO comment
 		private class CustomCategoryListOnItemClickListener implements OnItemClickListener
 		{
 			private int categoryPosition;
@@ -227,87 +241,92 @@ public class LiveTrackingFragment extends SherlockFragment {
 		}
 	}
 
+	//Starten, Anhalten und Auswerten von Tracking
 	public void changeTrackingState(View view) {
+		// Container Activity
 		MenuContainerActivity ac = (MenuContainerActivity) getActivity();
+
+		// Start Tracking
 		if (view.getTag() == null) {
+			
+			//Koordinaten des vergangenen Tracking aus DB entfernen
 			db.clearCoordinates();
+			
+			//Tracking über GPS oder Netzwerk starten
 			gps = new GPSTracker(mContext, this);
 			view.setTag(1);
-			((TextView) view).setText(getString(R.string.button_workout_stop));
 			
-			//Lock navigation to achievements during tracking
+			//Überschrift von Start auf Stop aendern
+			((TextView) view).setText(getString(R.string.button_workout_stop));
+
+			// Achievements und Menü sperren solange Tracking aktiv
 			ac.setLocked(true);
 			ac.invalidateOptionsMenu();
-			
-		} else if ((Integer) view.getTag() == 1) {
-			if (gps.canGetLocation()) {
-				
-				List <Coordinates> listContents = new ArrayList<Coordinates>();
-				listContents = db.getAllCoordinatePairs();
-				// TODO: Fix Lists
-				List <Workout> listWorkouts = new ArrayList<Workout>();
-				listWorkouts = db.getAllWorkouts();
-				
-				
-				for (i = 0; i < listContents.size(); i++) {
-					Toast.makeText(
-							mContext,
-							"Your Location is - \nLon: "
-									+ listContents.get(i).get_longitude()
-									+ "\nLat: "
-									+ listContents.get(i).get_latitude()
-									+ "\nAlt: "
-									+ GPSTracker.getElevationFromGoogleMaps(
-											listContents.get(i).get_longitude(),
-											listContents.get(i).get_latitude())
-								    + "\nDuration"
-								    + TrackService.calcDuration(listContents)
-								    + "\nNumberOfWorkouts: "
-								    + listWorkouts.size()
-							        + "\nElevation"
-							        + TrackService.calcElevation(listContents)
-									+ "\nDistance: "
-									+ TrackService.calcDistance(listContents),
-							Toast.LENGTH_LONG).show();
-				}
-			}else {
-				gps.showSettingsAlert();
-			}
+
+		} 
+		else if ((Integer) view.getTag() == 1) {
 			view.setTag(2);
 			
-			mView.findViewById(R.id.workout_layout).setVisibility(View.GONE);
-			MapView mapView = (MapView) mView.findViewById(R.id.mapview);
-			mapView.setVisibility(View.VISIBLE);
-			mapView.setBuiltInZoomControls(true);
-			Drawable marker=getResources().getDrawable(android.R.drawable.star_big_on);
-	        int markerWidth = marker.getIntrinsicWidth();
-	        int markerHeight = marker.getIntrinsicHeight();
-	        marker.setBounds(0, markerHeight, markerWidth, 0);
-	         
-	        ResourceProxy resourceProxy = new DefaultResourceProxyImpl(mContext);
-	         
-	        myItemizedOverlay = new MyItemizedOverlay(marker, resourceProxy);
-	        mapView.getOverlays().add(myItemizedOverlay);
-			List <Coordinates> listContents = new ArrayList<Coordinates>();
-			listContents = db.getAllCoordinatePairs();
-			for (Coordinates i:listContents){
-				GeoPoint myPoint1 = new GeoPoint(i.get_latitude(), i.get_longitude());
-				myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
-			}
-			
+			//Tracking anhalten
 			gps.stopUsingGPS();
+			
+			//Route auf Karte anzeigen
+			showTrackingRouteOnMap();
+
+			//Neues Workout speicern
 			Workout aktWorkout = populateWorkoutWithData();
 			db.addWorkout(aktWorkout);
 			db.checkAchievements(aktWorkout, mContext);
-			
-			((TextView) view).setText(getString(R.string.button_workout_analyse));
-		} else if ((Integer) view.getTag() == 2) {
+			((TextView) view)
+					.setText(getString(R.string.button_workout_analyse));
+		} 
+		
+		//Navigation zur Detailauswertung 
+		else if ((Integer) view.getTag() == 2) {
 			getToTrackingEvaluation();
+			//Menü und Achievements reaktivieren
 			ac.setLocked(false);
 			ac.invalidateOptionsMenu();
 		}
 	}
 	
+	
+	//Route auf Karte anzeigen
+	public void showTrackingRouteOnMap(){
+		//Karte einblenden
+		mView.findViewById(R.id.workout_layout).setVisibility(View.GONE);
+		MapView mapView = (MapView) mView.findViewById(R.id.mapview);
+		mapView.setVisibility(View.VISIBLE);
+		
+		//Zoom Funktionalität aktivieren
+		mapView.setBuiltInZoomControls(true);
+		
+		//Markierungelement definieren
+		Drawable marker = getResources().getDrawable(
+				android.R.drawable.star_big_on);
+		int markerWidth = marker.getIntrinsicWidth();
+		int markerHeight = marker.getIntrinsicHeight();
+		marker.setBounds(0, markerHeight, markerWidth, 0);
+		
+		
+		//Markierungselement auf Karte positionieren
+		ResourceProxy resourceProxy = new DefaultResourceProxyImpl(mContext);
+		MyItemizedOverlay myItemizedOverlay = new MyItemizedOverlay(marker,
+				resourceProxy);
+		mapView.getOverlays().add(myItemizedOverlay);
+		List<Coordinates> listContents = new ArrayList<Coordinates>();
+		listContents = db.getAllCoordinatePairs();
+		
+		//Koordinaten als Markierungselement auf Karte anzeigen
+		for (Coordinates i : listContents) {
+			GeoPoint myPoint1 = new GeoPoint(i.get_latitude(),
+					i.get_longitude());
+			myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
+		}
+		
+	}
+	
+	//Neues Workout aus aktuellen Trackingergebnissen erstellen
 	public Workout populateWorkoutWithData(){
 		List <Coordinates> listContents = new ArrayList<Coordinates>();
 		listContents = db.getAllCoordinatePairs();
@@ -325,7 +344,11 @@ public class LiveTrackingFragment extends SherlockFragment {
 				.add(R.id.currentFragment, single_evaluation).commit();
 	}
 
+	
+	//Zur Detailauswertung
 	public void getToTrackingEvaluation() {
+		
+		//TODO comment
 		Bundle bundle = new Bundle();
 		bundle.putInt("segmentlength", mSegmentList.size());
 		for (int i=0; i<mSegmentList.size(); i++)
